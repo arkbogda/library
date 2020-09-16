93b8bc9af2f45b071622c5037e038996db929e0e
Arkadiusz <abogdan@ittouch.pl>
2020-09-16 16:08:31 +0200

    Enum v1.4

--- src/pl/ab/library/app/LibraryApp.java
+++ src/pl/ab/library/app/LibraryApp.java
@@ -11,5 +11,4 @@ public class LibraryApp {
         libraryControl.controlLoop();
 
     }
-
 }
--- src/pl/ab/library/app/LibraryControl.java
+++ src/pl/ab/library/app/LibraryControl.java
@@ -6,20 +6,15 @@ import pl.ab.library.model.Library;
 import pl.ab.library.model.Magazine;
 
 public class LibraryControl {
-    private static final int EXIT = 0;
-    private static final int ADD_BOOK = 1;
-    private static final int ADD_MAGAZINES = 2;
-    private static final int PRINT_BOOKS = 3;
-    private static final int PRINT_MAGAZINES = 4;
     private DataReader dataReader = new DataReader();
     private Library library = new Library();
 
     public void controlLoop() {
-        int option;
+        Option option;
 
         do {
             printOptions();
-            option = dataReader.getInt();
+            option = Option.createFromInt(dataReader.getInt());
             switch (option) {
                 case ADD_BOOK:
                     addBook();
@@ -27,7 +22,7 @@ public class LibraryControl {
                 case PRINT_BOOKS:
                     printBooks();
                     break;
-                case ADD_MAGAZINES:
+                case ADD_MAGAZIN:
                     addMagazine();
                     break;
                 case PRINT_MAGAZINES:
@@ -39,7 +34,7 @@ public class LibraryControl {
                 default:
                     System.out.println("Nie ma takiej opcji, wprowadź ponownie");
             }
-        } while(option != 0);
+        } while(option != Option.EXIT);
     }
 
     private void printMagazines() {
@@ -67,10 +62,8 @@ public class LibraryControl {
 
     private void printOptions() {
         System.out.println("Wybierz opcję:");
-        System.out.println(EXIT + " - wyjście z programu");
-        System.out.println(ADD_BOOK + " - dodanie nowej książki");
-        System.out.println(ADD_MAGAZINES + " - dodanie nowego magazynu");
-        System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
-        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
+        for (Option value : Option.values()) {
+            System.out.println(value);
+        }
     }
 }
