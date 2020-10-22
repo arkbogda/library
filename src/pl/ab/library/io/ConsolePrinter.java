f9ba3245d4c2651b1ff6f6f052a5ed7dc73567b9
Arkadiusz <abogdan@ittouch.pl>
2020-10-22 14:22:28 +0200

    Collection processing optimization v2.6

--- src/pl/ab/library/io/ConsolePrinter.java
+++ src/pl/ab/library/io/ConsolePrinter.java
@@ -7,27 +7,25 @@ import java.util.Collection;
 public class ConsolePrinter {
 
     public void printBooks(Collection<Publication> publications) {
-        int countBooks = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Book)
-                printLine(publication.toString());
-            countBooks++;
-        }
-        if (countBooks == 0) {
-            printLine("Brak książek");
+        long count = publications.stream()
+                .filter(p -> p instanceof Book)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0) {
+            printLine("Brak książek w bibliotece");
         }
     }
 
 
     public void printMagazines(Collection<Publication> publications) {
-        int countMagazines = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Magazine)
-                printLine(publication.toString());
-            countMagazines++;
-        }
-        if (countMagazines == 0) {
-            printLine("Brak magazynów");
+        long count = publications.stream()
+                .filter(p -> p instanceof Magazine)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0) {
+            printLine("Brak magazynów w bibliotece");
         }
     }
 
@@ -35,6 +33,9 @@ public class ConsolePrinter {
         for (LibraryUser user : users) {
             printLine(user.toString());
         }
+        users.stream()
+                .map(User::toString)
+                .forEach(this::printLine);
     }
 
     public void printLine(String text) {
